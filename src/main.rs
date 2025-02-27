87f097714e248b809b771ba8e5eb9aa6e3f94528
David Sánchez <davidslt+git@pm.me>
2025-02-27 17:57:18 +0000

    refactor: code cleanup
    - Remove `lazy_static` dependency.
    - Use `PathBuf`s instead of strings where appropriate in CLI args parse.
    - Move template outside `src` directory.
    - Reuse code in golden test.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,13 @@
 //! of the dependencies of a project.
 //!
 //! The tool takes a JSON string with the dependency metadata as output by `cargo deny --manifest-path <PATH_TO_CARGO_TOML> list -l crate -f json` and generates a markdown file with the relevant information.
-extern crate tera;
-#[macro_use]
-extern crate lazy_static;
 
 use serde_json::{Map, Value};
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 use tera::{Context, Tera};
 
 use clap::Parser;
@@ -24,31 +22,19 @@ struct Args {
     #[arg(short, long)]
     dependencies: String,
     #[arg(short, long)]
-    #[clap(default_value = "src/templates/*")]
-    template_path: String,
+    template_path: PathBuf,
     #[arg(short, long)]
     #[clap(default_value = "THIRD_PARTY_NOTICES.md")]
-    output_file: String,
-}
-
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let args = Args::parse();
-        let tpl_path = args.template_path;
-        match Tera::new(&tpl_path) {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Parsing error(s): {}", e);
-                ::std::process::exit(1);
-            }
-        }
-    };
+    output_file: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+
+    let templates = Tera::new(&args.template_path.to_string_lossy())?;
+
     let mut f = File::create(args.output_file)?;
-    let markdown = render_markdown(args.dependencies);
+    let markdown = render_markdown(args.dependencies, &templates);
     match markdown {
         Ok(s) => Ok(f.write_all(s.as_bytes())?),
         Err(e) => {
@@ -63,7 +49,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 }
 
-fn render_markdown(data: String) -> Result<String, Box<dyn Error>> {
+fn render_markdown(data: String, templates: &Tera) -> Result<String, Box<dyn Error>> {
     let serialized = serde_json::from_str::<Map<String, Value>>(data.as_str()).unwrap();
 
     let mut seen = HashSet::new();
@@ -84,5 +70,5 @@ fn render_markdown(data: String) -> Result<String, Box<dyn Error>> {
     // A one off template
     Tera::one_off("hello", &Context::new(), true).unwrap();
 
-    Ok(TEMPLATES.render("THIRD_PARTY_NOTICES.md.tmpl", &context)?)
+    Ok(templates.render("THIRD_PARTY_NOTICES.md.tmpl", &context)?)
 }
